5b68fa7500628a80558c0935697c4e03aa3331ab
Nimesha Dilini <nimeshadilini999@gmail.com>
2020-11-09 23:52:24 +0530

    added the producer module

---
+++ org.wso2.carbon.greeting.card.producer/src/main/java/org/wso2/carbon/greeting/card/producer/GreetingCardProducer.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.greeting.card.producer;
+
+public interface GreetingCardProducer {
+    void createGreetingCard(String cardName, String greeting);
+
+    void listGreetingCards();
+}
